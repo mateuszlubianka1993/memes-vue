0aff75ac96611898881b6f312c2ef8480fbad7eb
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-06-17 12:39:17 +0200

    Install MDBVue and add SignUp component.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/lib/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
