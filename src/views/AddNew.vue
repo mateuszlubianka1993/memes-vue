7b60a35663fa20be6d3830abe931bfa67de53f3d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-06-24 16:29:29 +0200

    Add onSelect finction.

--- src/views/AddNew.vue
+++ src/views/AddNew.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <form class="add-form col-10 col-md-8 col-lg-6 m-auto">
+      <form class="add-form col-10 col-md-8 col-lg-6 m-auto border border-primary p-3">
         <p class="h4 text-center mb-4">Add new meme.</p>
         <div class="grey-text">
           <mdb-input type="text" label="Meme title" bg counter />
@@ -18,6 +18,7 @@
                 class="custom-file-input"
                 id="inputGroupFile01"
                 aria-describedby="inputGroupFileAddon01"
+                @change="onSelect"
               />
               <label class="custom-file-label" for="inputGroupFile01"
                 >Choose file</label
@@ -41,6 +42,17 @@ export default {
     mdbInput,
     mdbBtn,
   },
+  data() {
+      return {
+          file: null
+      }
+  },
+  methods: {
+      onSelect(e) {
+          this.file = e.target.files[0];
+          console.log(this.file)
+      }
+  }
 };
 </script>
 
